90b0a2c3b64bedd2c9975c52bd12293977a3edfc
SN-Vidya <snvidya1998@gmail.com>
2021-05-14 14:16:28 +0530

    ques7

---
+++ JAVACore/src/Array.java
@@ -0,0 +1,19 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		int[] myArray = {5,12,14,6,78,19,1,23,26,35,37,7,52,86,47};
+		int value=52;
+		
+		for(int i=0;i<myArray.length;i++){
+			if(value==myArray[i]) {
+				
+    	       System.out.println("array contains the given value");
+    	      
+    		 
+			}
+		}
+    	  
+	}
+
+}
