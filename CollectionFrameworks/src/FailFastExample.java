538ca1053abd807cba945db7fa7fba01a784f252
SN-Vidya <snvidya1998@gmail.com>
2021-05-14 16:57:07 +0530

    failfast,failsafe

---
+++ CollectionFrameworks/src/FailFastExample.java
@@ -0,0 +1,23 @@
+
+		import java.util.HashMap;   
+	    import java.util.Iterator;   
+	    import java.util.Map;   
+	    class FailFastDemo {  
+	        public static void main(String[] args)   
+	        {   
+	            Map<String, String> empName = new HashMap<String, String>();   
+	            empName.put("Sam Hanks", "New york");   
+	            empName.put("Will Smith", "LA");   
+	            empName.put("Scarlett", "Chicago");   
+	            Iterator iterator = empName.keySet().iterator();   
+	            while (iterator.hasNext()) {   
+	                System.out.println(empName.get(iterator.next()));   
+	                  
+	                empName.put("Istanbul", "Turkey");   
+	            }   
+	        }   
+	     
+
+	}
+
+
